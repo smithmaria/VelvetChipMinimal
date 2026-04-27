69d72057432e039acec498f90817a245527dc6cc
Maria Smith <maria.t.smith023@gmail.com>
2026-04-26 22:12:17 -0400

    order list styling

--- src/components/OrderInfo.jsx
+++ src/components/OrderInfo.jsx
@@ -3,7 +3,10 @@ import './OrderInfo.css'
 function OrderInfo() {
   return (
     <div className="order-container">
-      <h1>Order Info</h1>
+      <div>
+        <h1>Order Info</h1>
+      </div>
+      <button className='place-order'>Place Order</button>
     </div>
   )
 }
