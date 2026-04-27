e4c3f03288751e2ed82a9f0798087bafc2204862
Maria Smith <maria.t.smith023@gmail.com>
2026-04-26 23:03:26 -0400

    add order item card

--- src/components/OrderInfo/OrderInfo.jsx
+++ src/components/OrderInfo/OrderInfo.jsx
@@ -5,6 +5,15 @@ function OrderInfo() {
     <div className="order-container">
       <div>
         <h1>Order Info</h1>
+        <div className='order-list'>
+          <div className='order-item'>
+            <div>
+              <h3>Product Name</h3>
+              $9.99
+            </div>
+            <button className='remove'>Remove</button>
+          </div>
+        </div>
       </div>
       <button className='place-order'>Place Order</button>
     </div>
